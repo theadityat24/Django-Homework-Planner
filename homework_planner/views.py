c3037058a3fd8f3ed487069945cdca48b6560195
Aditya Tiwari <theadityat24@gmail.com>
2019-10-09 14:29:27 -0500

    created models

--- homework_planner/views.py
+++ homework_planner/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def assignments(request):
+def assignments (request):
     return HttpResponse("<i>My first website print</i>")
 
 def classWork(request):
