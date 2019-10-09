194e7a019aff4ece489cb5c9ed1bf463da3fc8b1
Aditya Tiwari <theadityat24@gmail.com>
2019-10-08 20:09:21 -0500

    Added views

--- homework_planner/views.py
+++ homework_planner/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def assignments(request):
+    return HttpResponse("<i>My first website print</i>")
+
+def classWork(request):
+    return HttpResponse("<button>classWork</button>")
+
+def ultraSecret(request):
+    return HttpResponse("This is a very secret server wow u accesed it!")
